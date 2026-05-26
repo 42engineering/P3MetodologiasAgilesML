5278c90c14f5ecff287201c74e63c6760d71201a
42engineering <42engineering.life@gmail.com>
2026-05-26 05:55:24 +0300

    Included script data_acquisition_tesla.py

---
+++ tdsp_template/P3_tsla/main.py
@@ -0,0 +1,11 @@
+from scripts.data_acquisition.data_acquisition_tsla import dataTeslaCsv
+
+def main():
+    
+    df = dataTeslaCsv()
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
+    
---
+++ tdsp_template/P3_tsla/scripts/data_acquisition/data_acquisition_tsla.py
@@ -0,0 +1,31 @@
+import os
+import yfinance as yf
+import pandas as pd
+
+
+def dataTeslaCsv():
+
+    filePath = "data/raw/TeslaData.csv"
+    os.makedirs("data/raw", exist_ok=True)
+
+    if os.path.exists(filePath):
+        print(f"El archivo ya existe: {filePath}")
+        df = pd.read_csv(filePath)
+
+        return df
+
+    print("Descargando datos desde Yahoo Finance...")
+    df = yf.download(
+        "TSLA",
+        start="2015-01-01",
+        end="2025-12-31"
+    )
+
+    df.to_csv(filePath)
+    print(f"Archivo guardado en: {filePath}")
+    return df
+
+
+if __name__ == "__main__":
+    df = dataTeslaCsv()
+    print(df.head())
