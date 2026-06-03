fd4164d7de20ab08f953de333e70fadc51852d67
42engineering <42engineering.life@gmail.com>
2026-06-04 00:09:44 +0300

    Updated constants.py with path for model in format keras

--- tdsp_template/P3_tsla/api/constants.py
+++ tdsp_template/P3_tsla/api/constants.py
@@ -9,25 +9,22 @@ CSV_PATH = (
     "TeslaData.csv"
 )
 
-PIPELINE_NAME = (
-    "Pipeline DesplieguePT1.1NE"
-)
-
-MODEL_NAME = (
-    "PT1_1_TSLA_30DAYS_LSTM.h5"
-)
-
 MODEL_PATH = (
-    "models/"
-    "PT1_1_TSLA_30DAYS_LSTM.h5"
+    P3_TSLA_ROOT /
+    "models" /
+    "PT1_1_TSLA_30DAYS_LSTM.keras"
 )
+
+PIPELINE_NAME = "Pipeline DesplieguePT1.1NE"
+
+MODEL_NAME = "PT1_1_TSLA_30DAYS_LSTM.keras"
 
 SEQUENCE_LENGTH = 30
 
 FEATURES = [
-    'Open',
-    'High',
-    'Low',
-    'Close',
-    'Volume'
+    "Open",
+    "High",
+    "Low",
+    "Close",
+    "Volume"
 ]
